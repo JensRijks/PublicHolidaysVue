376da20b40ddb9d1049c82f16a7153fdddce81a3
JensRijks <jens.rijks123@gmail.com>
2024-12-11 14:50:35 +0100

    new vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/PublicHolidaysVue/',
   plugins: [vue(), vueDevTools()],
   server: {
     port: 3000,
